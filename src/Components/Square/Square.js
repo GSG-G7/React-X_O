57ab87b6ed539f75a01f03fcfa293d6e526b8af2
mossa-Sammer <mossa708496@gmail.com>
2019-09-11 13:53:26 +0300

    passing data done

--- src/Components/Square/Square.js
+++ src/Components/Square/Square.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React from "react";
 
 export default class Square extends React.Component {
+  state = {
+    value: null
+  };
+
+  onclickHandler = () => {
+    this.setState({ value: "X" });
+  };
+
   render() {
-    return <button className='square'>{/* TODO */}</button>;
+    return (
+      <button className="square" onClick={this.onclickHandler}>
+        {this.state.value}
+      </button>
+    );
   }
 }
