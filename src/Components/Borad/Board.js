11c346c9c01c8ef9b335077d24febede19e71739
Mohammed <mhmmade@gmail.com>
2019-09-11 14:52:31 +0300

    lifting up state realtes #10

--- src/Components/Borad/Board.js
+++ src/Components/Borad/Board.js
@@ -1,27 +1,45 @@
-import React from "react";
-import Square from "../Square/Square";
+import React from 'react';
+import Square from '../Square/Square';
 export default class Board extends React.Component {
+  state = {
+    squars: Array(9).fill(null)
+  };
+
+  onclickHandler = index => {
+    const squars = [...this.state.squars];
+
+    alert(index);
+    // this.setState({ value: 'X' });
+  };
+
   renderSquare(i) {
-    return <Square value={i} />;
+    // console.log(i);
+    return (
+      <Square
+        onclickHandler={this.onclickHandler}
+        value={this.state.squars[i]}
+        index={i}
+      />
+    );
   }
 
   render() {
-    const status = "Next player: X";
+    const status = 'Next player: X';
 
     return (
       <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
+        <div className='status'>{status}</div>
+        <div className='board-row'>
           {this.renderSquare(0)}
           {this.renderSquare(1)}
           {this.renderSquare(2)}
         </div>
-        <div className="board-row">
+        <div className='board-row'>
           {this.renderSquare(3)}
           {this.renderSquare(4)}
           {this.renderSquare(5)}
         </div>
-        <div className="board-row">
+        <div className='board-row'>
           {this.renderSquare(6)}
           {this.renderSquare(7)}
           {this.renderSquare(8)}
