8095aa39eb1da6c684e7581ee365c3740ad424db
Mohammed <mhmmade@gmail.com>
2019-09-12 14:05:58 +0300

    fix onclick hanlder relates #21

--- src/utils/calcWinner.js
+++ src/utils/calcWinner.js
@@ -1,5 +1,4 @@
 const calcWinner = squares => {
-  console.log(squares);
   const lines = [
     [0, 1, 2],
     [3, 4, 5],
@@ -13,7 +12,7 @@ const calcWinner = squares => {
 
   for (let i = 0; i < lines.length; ++i) {
     const [a, b, c] = lines[i];
-    console.log(a);
+
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
     }
