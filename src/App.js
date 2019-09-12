8f305552a70394763da3e74ff46053a1dcdb3474
Mohammed <mhmmade@gmail.com>
2019-09-12 10:56:13 +0300

    refator code based on best practise & modulrize the code realates #15

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import Game from "./Components/ Game/ Game"
+import Game from './container/ Game/ Game';
 function App() {
   return (
     <React.Fragment>
       <Game />
     </React.Fragment>
-  )
+  );
 }
 
 export default App;
